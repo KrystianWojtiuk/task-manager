0892e4026964b0d4d675b1a8c87a9012aea52b6e
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-05-19 22:42:02 +0100

    Add __str__ method for each model

--- tasks/models.py
+++ tasks/models.py
@@ -5,10 +5,16 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Worker(AbstractUser):
@@ -26,15 +32,21 @@ class Project(models.Model):
     is_complete = models.BooleanField(default=False)
     teams = models.ManyToManyField(Team, related_name='projects')
 
+    def __str__(self):
+        return self.name
+
 
 class TaskType(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
-        ('Urgent', 'High'),
-        ('Normal', 'Low'),
+        ('Urgent', 'Urgent'), ('High', 'High'),
+        ('Normal', 'Normal'), ('Low', 'Low')
     ]
 
     name = models.CharField(max_length=100)
@@ -49,3 +61,6 @@ class Task(models.Model):
     task_type = models.ForeignKey(TaskType, on_delete=models.PROTECT)
     assignees = models.ManyToManyField(Worker, related_name='tasks', blank=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
