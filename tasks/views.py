76a52a86b9ddba1d59d35a02a1111f5fc2f1c778
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-05-20 03:11:31 +0100

    Update index page update index view add index.html template

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from tasks.models import Worker, Task, Project
 
 
 def index(request):
-    return HttpResponse("Index Page")
+    num_workers = Worker.objects.count()
+    num_projects = Project.objects.count()
+    num_tasks = Task.objects.count()
+    context = {
+        "num_workers": num_workers,
+        "num_projects": num_projects,
+        "num_tasks": num_tasks,
+    }
+    return render(request, "tasks/index.html", context=context)
