d84fc55fd391cde2047fef7c4dba8ef38799e4eb
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-08-27 14:44:50 +0100

    Remove debug toolbar

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("manager/", include("manager.urls", namespace="manager")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
