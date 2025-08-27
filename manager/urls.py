220336370dd1727b25bb0f91eea40858de2a049c
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-08-27 12:03:00 +0100

    Change list views urls

--- manager/urls.py
+++ manager/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from manager.views import index, WorkerListView, TaskListView, ProjectListView
+from manager.views import index, WorkerListView, TaskListView, ProjectListView, WorkerDetailView
 
 app_name = "manager"
 
 urlpatterns = [
     path("", index, name="index"),
-    path("worker-list/", WorkerListView.as_view(), name="worker-list"),
-    path("project-list/", ProjectListView.as_view(), name="project-list"),
-    path("task-list/", TaskListView.as_view(), name="task-list"),
+    path("workers/", WorkerListView.as_view(), name="workers"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("projects/", ProjectListView.as_view(), name="projects"),
+    path("tasks/", TaskListView.as_view(), name="tasks"),
 ]
