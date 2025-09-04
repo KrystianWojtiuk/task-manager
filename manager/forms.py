8d8857f1f5d0f81b5076f303397ed844f87f7543
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-09-04 17:54:24 +0100

    Add HTML5 datetime-local input for deadline in project and task form

--- manager/forms.py
+++ manager/forms.py
@@ -5,7 +5,21 @@ from .models import Project, Task
 
 
 class DeadlineValidationForm(forms.ModelForm):
-    """Base form to validate that deadline is at least 1 day from now if incomplete."""
+    """Base form to validate that deadline is at least 1 day from now if incomplete,
+    and render a HTML5 datetime picker."""
+
+    class Meta:
+        widgets = {
+            "deadline": forms.DateTimeInput(
+                attrs={"type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M"
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.deadline:
+            self.initial['deadline'] = self.instance.deadline.strftime("%Y-%m-%dT%H:%M")
 
     def clean(self):
         cleaned_data = super().clean()
